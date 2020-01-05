5ffc474778bc088b5ef216e03946323633d35368
jqyp <jqy@protonmail.com>
2020-01-05 05:00:22 +0300

    Solve homework03

--- hw03/task2.sql
+++ hw03/task2.sql
@@ -3,3 +3,8 @@
 --
 -- Результат
 -- name TEXT
+
+SELECT Planet.name
+FROM Planet
+LEFT JOIN Flight ON Planet.id = Flight.planet_id
+WHERE Flight.id IS NULL;
--- hw03/task6.sql
+++ hw03/task6.sql
@@ -5,3 +5,12 @@
 --
 -- Результат
 -- spacecraft_id INT, spacecraft_name TEXT, avg_distance NUMERIC
+
+SELECT Spacecraft.id AS spacecraft_id,
+       Spacecraft.name AS spacecraft_name,
+       AVG(Planet.distance) AS avg_distance
+FROM Spacecraft
+JOIN Flight ON Spacecraft.id = Flight.spacecraft_id
+JOIN Planet ON Flight.planet_id = Planet.id
+GROUP BY Spacecraft.id
+HAVING AVG(Planet.distance) > 500;
